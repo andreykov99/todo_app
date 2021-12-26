8f8e16cb44f6bba7bddaf962d2e36b8478c1f782
Andrey Kovalevskij <andrey.kovalevskij@gmail.com>
2021-12-26 16:34:13 +0200

    some edit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders add todo link", () => {
   render(<App />);
   const linkElement = screen.getByText(/add todo/i);
   expect(linkElement).toBeInTheDocument();
